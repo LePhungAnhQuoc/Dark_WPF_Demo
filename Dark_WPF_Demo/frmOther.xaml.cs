ec93b668cf537f0c39a2423a1768add9ad6b5b3f
Your Name <you@example.com>
2023-05-30 12:18:13 +0700

    Validate number

--- Dark_WPF_Demo/frmOther.xaml.cs
+++ Dark_WPF_Demo/frmOther.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -38,6 +39,14 @@ namespace Dark_WPF_Demo
         {
             InitializeComponent();
             btnCreate.Click += new RoutedEventHandler(BtnCreate_Click);
+            txtRow.PreviewTextInput += TextBox_PreviewTextInput;
+            txtColumn.PreviewTextInput += TextBox_PreviewTextInput;
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
 
         public void Init(List<List<Button>> source)
